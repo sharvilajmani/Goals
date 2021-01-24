346d6d4689bdbd2905bbfa88259898bdbf32a110
Sharvil Ajmani <sharvilajmani92@gmail.com>
2021-01-25 01:27:11 +0530

    My First Commit

---
+++ components/GoalItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+const GoalItem = ({ onDelete, title, id }) => {
+  return (
+    <TouchableOpacity onPress={onDelete.bind(this, id)}>
+      <View style={styles.listItem}>
+        <Text>{title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
+  },
+});
+
+export default GoalItem;
